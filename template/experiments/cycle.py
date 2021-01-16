a0d99b3d425a53f18e8a7f1201f195dd20c3d97a
Eran Agmon <agmon.eran@gmail.com>
2021-01-15 17:32:03 -0800

    import more processes

--- template/experiments/cycle.py
+++ template/experiments/cycle.py
@@ -48,14 +48,88 @@ required processes:
     *
 """
 
+from vivarium.core.process import (
+    Process,
+    Deriver,
+    Composite)
+
 # helper functions for composition
 from vivarium.core.composition import (
     compose_experiment,
     FACTORY_KEY,
 )
 
+from vivarium.processes.meta_division import MetaDivision
+from vivarium.processes.divide_condition import DivideCondition
+from vivarium.processes.burst import Burst
 
-def Cell
+
+class Growth(Process):
+    defaults = {}
+
+    def ports_schema(self):
+        return {
+            'biomass': {
+                '_default': 1,
+                '_emit': True,
+            }
+        }
+
+    def next_update(self, timestep, states):
+        return {}
+
+
+class Replication(Process):
+    defaults = {}
+
+    def ports_schema(self):
+        return {
+            'genes': {
+                '*': {
+                    'state': 'active',
+                    'number': 1,
+                }
+            }
+        }
+
+    def next_update(self, timestep, states):
+        return {}
+
+
+class Cell(Composite):
+    defaults = {
+        'growth': {},
+        'replication': {},
+        'daughter_path': tuple()
+    }
+
+    def generate_processes(self, config):
+
+        # division config
+        daughter_path = config['daughter_path']
+        agent_id = config['agent_id']
+        division_config = dict(
+            config.get('division', {}),
+            daughter_path=daughter_path,
+            agent_id=agent_id,
+            generator=self)
+
+        return {
+            'growth': Growth(config['growth']),
+            'replication': Replication(config['replication']),
+            'divide_condition': DivideCondition(),
+            'meta_division': MetaDivision(division_config),
+            'burst': Burst(),
+        }
+
+    def generate_topology(self, config):
+        return {
+            'growth': {},
+            'replication': {},
+            'divide_condition': {},
+            'meta_division': {},
+            'burst': {}
+        }
 
 
 
